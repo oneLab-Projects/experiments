2545eb058ec137421adddadb19f7802e8c8e98ee
Semyon Butenko <laim0n@yandex.ru>
2020-07-02 16:23:41 +0300

    Updated theme

--- lib/component/theme.dart
+++ lib/component/theme.dart
@@ -3,14 +3,14 @@ import 'package:pansy_ui/pansy_ui.dart';
 class AppTheme {
   static ThemeData dayTheme = ThemeData(
     primaryColor: Colors.grey[900],
-    buttonColor: Colors.grey[900],
+    buttonColor: Colors.grey[800],
     accentColor: Colors.blueGrey[900],
   );
 
   static ThemeData nightTheme = ThemeData(
     accentColor: Colors.blueGrey[100],
     primaryColor: Colors.grey[50],
-    buttonColor: Colors.blueGrey[700],
+    buttonColor: Colors.blueGrey[400],
     scaffoldBackgroundColor: Colors.blueGrey[900],
     brightness: Brightness.dark,
   );
