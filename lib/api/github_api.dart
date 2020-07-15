510c5ff061b8b902272ee20de7d2800dc260cf9e
Roman S <ua.romansyslikov@gmail.com>
2020-07-15 20:20:14 +0300

    Update github_api.dart

--- lib/api/github_api.dart
+++ lib/api/github_api.dart
@@ -3,6 +3,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:onelab_experiments/api/github_user.dart';
 
 import 'api_client.dart';
@@ -10,21 +11,13 @@ import 'package:http/http.dart' as http;
 
 class GithubApiClient extends ApiClient
 {
-  Future<GithubUser> getUserByID(int id) async {
-    final String url = "https://api.github.com/user/" + id.toString();
 
-    final http.Response result = await http.get(url);
-    if (result.statusCode != 200)
-      throw "Fuck GitHub!";
+  Future<GithubUser> getUserByID(int id) async {
+    final http.Response result = await http.get("https://api.github.com/user/${id}");
+    assert(result.statusCode == 200);
     return GithubUser.fromJson(jsonDecode(result.body));
   }
 
-  Future<List<GithubUser>> getUsersByIDs(List<int> idList) async {
-    List<GithubUser> users = List<GithubUser>();
-    await Future.forEach(
-      idList, 
-      (id) async => users.add(await getUserByID(id))
-    );
-    return users;
-  }
+  Future<List<GithubUser>> getUsersByIDs(List<int> ids) async
+    => await Future.wait(ids.map((id) async => await getUserByID(id)));
 } 
