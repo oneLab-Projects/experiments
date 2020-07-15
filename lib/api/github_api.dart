c71067b84dac821603256c523a7e77de923095bb
Semyon Butenko <laim0n@yandex.ru>
2020-07-15 20:51:09 +0300

    Refactor

--- lib/api/github_api.dart
+++ lib/api/github_api.dart
@@ -1,20 +1,18 @@
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:onelab_experiments/api/github_user.dart';
 
 import 'api_client.dart';
 import 'package:http/http.dart' as http;
 
-class GithubApiClient extends ApiClient
-{
-
+class GithubApiClient extends ApiClient {
   Future<GithubUser> getUserByID(int id) async {
-    final http.Response result = await http.get("https://api.github.com/user/${id}");
+    final http.Response result =
+        await http.get("https://api.github.com/user/$id");
     assert(result.statusCode == 200);
     return GithubUser.fromJson(jsonDecode(result.body));
   }
 
-  Future<List<GithubUser>> getUsersByIDs(List<int> ids) async
-    => await Future.wait(ids.map(getUserByID));
-} 
+  Future<List<GithubUser>> getUsersByIDs(List<int> ids) async =>
+      await Future.wait(ids.map(getUserByID));
+}
