e330398be214dd4f4f799b10cfcbff2534b8ba2d
Semyon Butenko <laim0n@yandex.ru>
2020-07-17 16:59:36 +0300

    Refactor

--- lib/projects/avatar/widget.dart
+++ lib/projects/avatar/widget.dart
@@ -14,11 +14,6 @@ class Avatar extends StatefulWidget {
 
 class _AvatarState extends State<Avatar> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return ClipOval(
       child: Stack(
